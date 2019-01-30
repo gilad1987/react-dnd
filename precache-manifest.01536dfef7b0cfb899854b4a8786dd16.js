af2cf24313270a405a3321be015b02f77cdc141f
Gilad takoni <gilad1987@gmail.com>
2019-01-30 09:37:18 +0200

    .

---
+++ precache-manifest.01536dfef7b0cfb899854b4a8786dd16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d498b998f00312d036b2",
+    "url": "/static/js/main.d498b998.chunk.js"
+  },
+  {
+    "revision": "c1a029e0b674f34d2293",
+    "url": "/static/js/1.c1a029e0.chunk.js"
+  },
+  {
+    "revision": "d498b998f00312d036b2",
+    "url": "/static/css/main.37092b90.chunk.css"
+  },
+  {
+    "revision": "337d63fefbac352801e5d2eeeb66065c",
+    "url": "/index.html"
+  }
+];
